2bd162547533ae784f364bd3e6a8ba1267923f1b
Stephen <stephen@tca0.nl>
2022-04-26 21:47:08 +0200

    populate client correctly

--- cluster.go
+++ cluster.go
@@ -7,22 +7,23 @@ import (
 type Cluster struct {
 	Name      string
 	State     string
-	NodePools []NodePool
-	Nodes     []Node
+	NodePools []*NodePool
+	Nodes     []*Node
+	client    *Client
 }
 
 type ClusterResult struct {
 	Clusters []*Cluster `json:"content"`
 }
 
-func (c *Client) ListClusters(maxSize int) (*ClusterResult, error) {
+func (c *Client) ListClusters(maxSize int, page int) (*ClusterResult, error) {
 
 	// TODO handle paging
 	var result *ClusterResult
 	resp, err := c.symbiosisAPI.R().
 		SetResult(&result).
 		ForceContentType("application/json").
-		Get(fmt.Sprintf("rest/v1/cluster?size=%d&page=0", maxSize))
+		Get(fmt.Sprintf("rest/v1/cluster?size=%d&page=%s", maxSize, page))
 
 	if err != nil {
 		return nil, err
@@ -36,6 +37,12 @@ func (c *Client) ListClusters(maxSize int) (*ClusterResult, error) {
 
 	if validated == nil {
 		return nil, nil
+	}
+
+	if len(result.Clusters) > 0 {
+		for _, cluster := range result.Clusters {
+			cluster.populateClient(c)
+		}
 	}
 
 	return validated.(*ClusterResult), nil
@@ -63,5 +70,17 @@ func (c *Client) DescribeCluster(name string) (*Cluster, error) {
 		return nil, nil
 	}
 
+	result.populateClient(c)
+
 	return validated.(*Cluster), nil
+}
+
+func (c *Cluster) populateClient(client *Client) {
+	c.client = client
+
+	if len(c.Nodes) > 0 {
+		for _, node := range c.Nodes {
+			node.client = client
+		}
+	}
 }
--- example/main.go
+++ example/main.go
@@ -13,7 +13,7 @@ func main() {
 		log.Fatalf("Error occurred: %s", err)
 	}
 
-	result, err := client.ListClusters(10)
+	result, err := client.ListClusters(10, 0)
 
 	if err != nil {
 		log.Fatalf("Call failed: %s", err)
@@ -34,19 +34,15 @@ func main() {
 		log.Printf("Cluster name: %s, Node pools: %d", describeCluster.Name, len(describeCluster.NodePools))
 		log.Printf("Total nodes in cluster: %d", poolCount)
 
-		node, err := client.DescribeNode(describeCluster.Nodes[0].Name)
+		for _, node := range cluster.Nodes {
 
-		if err != nil {
-			log.Fatalf("Describe failed: %s", err)
+			err := node.Recycle()
+
+			if err != nil {
+				log.Fatalf("Recycle failed (%s): %s", node.Name, err)
+			}
+
+			log.Printf("Recycled: %s", node.Name)
 		}
-
-		log.Printf("Node name: %s", node.Name)
-
-		err = node.Recycle()
-
-		if err != nil {
-			log.Fatalf("Recycle failed (%s): %s", node.Name, err)
-		}
-
 	}
 }
